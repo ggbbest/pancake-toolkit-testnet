649ad22add1b316e9896c850add0d09d1aadcfb6
ggbbest <www.ggb.best@gmail.com>
2022-01-06 20:08:44 +0900

    2nd

--- src/components/Image/tokens.ts
+++ src/components/Image/tokens.ts
@@ -6,7 +6,6 @@ const tokens = {
     symbol: "CAKE",
     address: {
       21004: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
-      97: "0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe",
     },
     decimals: 18,
     projectLink: "https://vpoker.c4ei.net/",
@@ -15,7 +14,6 @@ const tokens = {
     symbol: "wBNB",
     address: {
       21004: "0x85FD9144a9C0d31988e273412CB3F48Fd246a2c0",
-      97: "0xae13d989dac2f0debff460ac112a837c89baa7cd",
     },
     decimals: 18,
     projectLink: "https://vpoker.c4ei.net/",
@@ -24,7 +22,6 @@ const tokens = {
     symbol: "SYRUP",
     address: {
       21004: "0x8943C8139c875f47c6f613ad36fFB623F250425d",
-      97: "0xfE1e507CeB712BDe086f3579d2c03248b2dB77f9",
     },
     decimals: 18,
     projectLink: "https://vpoker.c4ei.net/",
